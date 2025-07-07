c9f44f11f55c1c419bd0612c77667e33aae17f21
ishpreet7226 <ishpreet7226@gmail.com>
2025-07-07 10:16:36 +0530

    Create even_number.cpp

---
+++ even.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num;
+    cout<<"enter your no:";
+
+    cin>>num;
+
+    if(num%2==0){
+        cout<<num<<" is an even. "<<endl;
+    }
+    else{
+        cout<<num<<" is an odd. "<<endl;
+    }
+}
